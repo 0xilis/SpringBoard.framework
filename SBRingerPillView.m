b10d7cc30096b42f7abdac888366ad5baace614e
Snoolie <109871561+0xilis@users.noreply.github.com>
2023-06-12 20:17:14 -0400

    more

--- SBRingerPillView.m
+++ SBRingerPillView.m
@@ -21,5 +21,33 @@
  _glyphView = [[BSUICAPackageView alloc] initWithPackageName:@"Mute" inBundle:[NSBundle bundleForClass:[self class]]];
  [self addSubview:_glyphView];
  //finish latr
+ 
+ /*
+ Another part of the init method
+ #if 0
+ UIColor* secondaryLabelColor = [UIColor secondaryLabelColor];
+ _silentModeLabel = [[UILabel alloc]init];
+ [_silentModeLabel setFont:[[BSUIFontProvider defaultFontProvider]preferredFontForTextStyle:UIFontTextStyleFootnote hiFontStyle:0x4]];
+ [_silentModeLabel setTextColor:secondaryLabelColor];
+ [_silentModeLabel setText:[[[NSBundle mainBundle]localizedStringForKey:@"RINGER_SILENT" value:@"" table:@"SpringBoard"]];
+ [self addSubview:_silentModeLabel];
+ _ringerLabel = [[UILabel alloc]init];
+ [_ringerLabel setFont:[[BSUIFontProvider defaultFontProvider]preferredFontForTextStyle:UIFontTextStyleFootnote hiFontStyle:0x4]];
+ [_ringerLabel setTextColor:secondaryLabelColor];
+ [_ringerLabel setText:[[[NSBundle mainBundle]localizedStringForKey:@"RINGER_VOLUME" value:@"" table:@"SpringBoard"]];
+ [self addSubview:_ringerLabel];
+ _onLabel = [[UILabel alloc]init];
+ [_onLabel setFont:[[BSUIFontProvider defaultFontProvider]preferredFontForTextStyle:UIFontTextStyleFootnote hiFontStyle:0x4]];
+ [_onLabel setTextColor:[UIColor tertiaryLabelColor]];
+ [_onLabel setText:[[[NSBundle mainBundle]localizedStringForKey:@"RINGER_ON" value:@"" table:@"SpringBoard"]];
+ _offLabel = [[UILabel alloc]init]
+ [_offLabel setFont:[[BSUIFontProvider defaultFontProvider]preferredFontForTextStyle:UIFontTextStyleFootnote hiFontStyle:0x4]];
+ [_offLabel setTextColor:[UIColor tertiaryLabelColor]];
+ [_offLabel setText:[[[NSBundle mainBundle]localizedStringForKey:@"RINGER_OFF" value:@"" table:@"SpringBoard"]];
+ _slider = [[SBRingerVolumeSliderView alloc]init];
+ [_slider setValue:0.7];
+ [self addSubview:_slider];
+ #endif
+ */
 }
 @end
